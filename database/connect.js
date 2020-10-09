5ccd867cbc9d9e8e20e061c0ed0bf308188b2124
idkwhattochoose#6147(discord) <kahaanistalking@gmail.com>
2020-10-09 14:59:44 -0500

    fix errors with config and file paths

--- database/connect.js
+++ database/connect.js
@@ -1,6 +1,6 @@
 const start = async () => {
     const mongoose = require('mongoose');
-    const configuration = require('../../../bot_config.json');
+    const configuration = require('../bot_config.json');
     await mongoose.connect(configuration.mongo_uri, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
         console.log(err);
     })
