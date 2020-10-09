9560d52a88da5c0d51c89eaee2a639abc7ed3df5
idkwhattochoose#6147(discord) <kahaanistalking@gmail.com>
2020-10-09 14:58:22 -0500

    database with mongodb

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { Base } = require('discord.js');
+const mongoBase = require('./database/connect');
 const BaseClient = require('./structures/Client');
 const client = new BaseClient();
 const configuration = require('./bot_config.json');
 client.start(configuration, `./commands`)
+mongoBase.start();
